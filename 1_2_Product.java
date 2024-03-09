9772d4add68dfba10fa295a5c8db92ab9bca0b82
Megha-P-Varghese <142739312+Megha-P-Varghese@users.noreply.github.com>
2024-03-09 23:26:50 +0530

    Add files via upload

---
+++ 1_2_Product.java
@@ -0,0 +1,26 @@
+import java.io.*;
+class Product
+{
+  public static void main(String args[])
+  {
+  System.out.println("Megha P Varghese");
+  DataInputStream din;
+  int x,y;
+  String s;
+  din = new DataInputStream(System.in);
+  try
+  {
+    System.out.println("\nenter the first number :");
+    s=din.readLine();
+    x=Integer.parseInt(s);
+    System.out.println("\nenter the second  number :");
+    s=din.readLine();
+    y=Integer.parseInt(s);
+    System.out.println("\nproduct of " +x+ " and " +y+ " is " + (x*y));
+   }
+     catch (Exception e)
+    {
+      System.out.println("Error : "+e); 
+     }
+   }
+}
