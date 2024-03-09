7664478afe5f40eeb705f75e29adbb372660144a
Megha-P-Varghese <142739312+Megha-P-Varghese@users.noreply.github.com>
2024-03-09 23:26:14 +0530

    Add files via upload

---
+++ 1_1_Welcome.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class Welcome
+{
+   public static void main(String args[])
+   {
+     System.out.println("Megha P Varghese");
+     System.out.println("Welcome to Java");
+   }
+}
